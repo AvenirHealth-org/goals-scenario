c5e29c284beaca8ea5764721a6fc00438983932a
elric <ewerst@avenirhealth.org>
2026-04-07 19:18:53 -0400

    Ruff check fixes

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,14 +1,16 @@
-from Tools.ImportPJNZ.Importer import GB_ImportProjectionFromFile
 
 #Constants for modvar tags
 import SpectrumCommon.Const.DP as dpconst
-import SpectrumCommon.Const.AM as amconst
 import SpectrumCommon.Const.HV as hvconst
-import SpectrumCommon.Const.RN as rnconst
+from Tools.ImportPJNZ.Importer import GB_ImportProjectionFromFile
+
 
 def test_import_projection_from_file():
     projection, param, epp_files, shiny90 = GB_ImportProjectionFromFile(r".\tests\test_data\SouthAfrica.PJNZ")
     assert projection is not None
     assert param.country == "ZAF"
-    assert type(projection[dpconst.DP_TFRTag])==list
-    assert type(projection[hvconst.HVARTCoverageByRGTag])==list
+    assert type(projection[dpconst.DP_TFRTag]) is list
+    assert type(projection[hvconst.HVARTCoverageByRGTag]) is list
+    # there is no shiny90 or epp files in this test PJNZ
+    assert shiny90 is None
+    assert epp_files == {}
