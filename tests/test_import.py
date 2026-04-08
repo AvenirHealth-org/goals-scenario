f982337a848563650b0ed7395be939b42a965950
elric <ewerst@avenirhealth.org>
2026-04-08 09:25:39 -0400

    Added pytest fixture for the test data path. Hopefully resolves errors when being called from different systems

--- tests/test_import.py
+++ tests/test_import.py
@@ -3,9 +3,8 @@ import SpectrumCommon.Const.DP as dpconst
 import SpectrumCommon.Const.HV as hvconst
 from Tools.ImportPJNZ.Importer import GB_ImportProjectionFromFile
 
-
-def test_import_projection_from_file():
-    projection, param, epp_files, shiny90 = GB_ImportProjectionFromFile(r".\tests\test_data\SouthAfrica.PJNZ")
+def test_import_from_file(test_dir):
+    projection, param, epp_files, shiny90 = GB_ImportProjectionFromFile(test_dir / "SouthAfrica.PJNZ")
     assert projection is not None
     assert param.country == "ZAF"
     assert type(projection[dpconst.DP_TFRTag]) is list
