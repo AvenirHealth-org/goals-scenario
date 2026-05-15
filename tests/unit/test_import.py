d1e135d92e637099cf69cafd01beca6fe4475107
Rob Ashton <robertashton94@gmail.com>
2026-05-15 16:44:21 +0100

    Bump numpy version number and avenir package versions

--- tests/unit/test_import.py
+++ tests/unit/test_import.py
@@ -26,7 +26,7 @@ def test_import_all_pjnz_files_in_test_dir(pjnz_path):
     assert projection is not None
     assert param.country
     assert isinstance(projection[dpconst.DP_TFRTag], list)
-    assert isinstance(projection[hvconst.HVARTCoverageByRGTag], list)
+    assert isinstance(projection[hvconst.HV_ARTCoverageByRGTag], list)
     assert isinstance(epp_files, dict)
     if shiny90 is not None:
         assert isinstance(shiny90, dict)
@@ -39,4 +39,4 @@ def test_import_all_pjnz_files_in_test_dir(pjnz_path):
     first_year = projection[pjconst.PJN_FirstYearTag]
     year_count = final_year - first_year + 1
     assert projection[dpconst.DP_TFRTag].shape == (year_count,)
-    assert projection[hvconst.HVARTCoverageByRGTag].shape == (3, 11, year_count)
+    assert projection[hvconst.HV_ARTCoverageByRGTag].shape == (3, 11, year_count)
